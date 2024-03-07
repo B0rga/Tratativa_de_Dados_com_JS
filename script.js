005916a7940f2b73257d0ee62058598a403b3f13
Pedro Borges de Jesus <pedro.borges.jesus@gmail.com>
2024-03-07 20:21:53 -0300

    Lógica aprimorada, classe e métodos adicionados

--- script.js
+++ script.js
@@ -1,50 +1,81 @@
-let nome = document.querySelector("#nome");
-let idade = document.querySelector("#idade");
-let linguagem = document.querySelector("#linguagem");
-let sim = document.querySelector("#yes")
-let nao = document.querySelector("#no")
-let res = document.querySelector("#res");
-let res2 = document.querySelector("#res2");
-let resultado = document.querySelector("#resultado");
-
-function Enviar(){  
-    
-    res.innerText = "";
-    res2.innerText = "";
-
-    if(nome.value == '' || idade.value == '' || linguagem.value == ''){
-        document.querySelector("#resultado").style.visibility = "visible";
-        res.innerText = "Preencha todos os campos!";
-        
+class Pessoa{
+    constructor(){
+        this.arrayPessoas =[];
     }
-    else if(idade.value>120 || idade.value<1){
-        document.querySelector("#resultado").style.visibility = "visible";
-        res.innerText = "Insira uma idade válida!";
-    }
-    else{
-        if(sim.checked == true){
-            resultado.style.visibility = "visible";
-            res.innerText = `Olá ${nome.value}, você tem ${idade.value} anos e já está aprendendo ${linguagem.value}!`;
-            res2.innerText = `\nQue bom que está gostando! Continue estudando e você terá muito sucesso.`;
 
-            nome.value='';
-            idade.value = '';
-            linguagem.value = '';
-            sim.checked = false;
+    LerDados(){
+        let pessoa = {};
+        pessoa.nome = document.querySelector("#nome").value;
+        pessoa.idade = Number(document.querySelector("#idade").value);
+        pessoa.linguagem = document.querySelector("#linguagem").value;
+
+        if(document.getElementsByName("option")[0].checked==true){
+            pessoa.programacao = 'Gosta';
         }
-        else if(nao.checked == true){
-            resultado.style.visibility = "visible";
-            res.innerText = `Olá ${nome.value}, você tem ${idade.value} anos e já está aprendendo ${linguagem.value}!`;
-            res2.innerText = `\nQue pena que não está gostando... Já tentou aprender outras linguagens?`;
-            
-            nome.value='';
-            idade.value = '';
-            linguagem.value = '';
-            no.checked = false;
+        else if(document.getElementsByName("option")[1].checked==true){
+            pessoa.programacao = 'Não gosta';
         }
         else{
-            resultado.style.visibility = "visible";
-            res.innerText = "Preencha todos os campos!";
+            pessoa.programacao = '';
         }
-    }    
+
+        return pessoa;
+    }
+
+    ElementoVisivel(){
+        document.querySelector("#resultado").style.visibility="visible";
+    }
+
+    LimparCampos(){
+        let campos = document.querySelectorAll(".campo");
+        campos.forEach((campo) => campo.value="");
+
+        let radios = document.querySelectorAll("input[type=radio]");
+        radios.forEach((radio) => radio.checked=false);
+    }
+
+    ValidarCampos(pessoa){
+        if(pessoa.nome=="" || pessoa.linguagem=="" || pessoa.programacao==""){
+            this.ElementoVisivel();
+            document.getElementsByTagName("p")[0].innerHTML = "Preencha todos os campos!";
+            document.getElementsByTagName("p")[1].innerHTML = "";
+            return false;
+        }
+        else if(pessoa.idade>100 || pessoa.idade<1){
+            this.ElementoVisivel();
+            document.getElementsByTagName("p")[0].innerHTML = "Insira uma idade válida!";
+            document.getElementsByTagName("p")[1].innerHTML = "";
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    AdicionarNaArray(pessoa){
+        this.arrayPessoas.push(pessoa);
+        console.log(this.arrayPessoas);
+    }
+
+    ExibirDados(pessoa){
+        this.ElementoVisivel();
+        document.getElementsByTagName("p")[0].innerHTML = `Olá ${pessoa.nome}, você tem ${pessoa.idade} anos e já está aprendendo ${pessoa.idade}!`;
+        if(pessoa.programacao == "Gosta"){
+            document.getElementsByTagName("p")[1].innerHTML = `\nQue bom que está gostando de programação! Continue estudando e você terá muito sucesso!`;
+        }
+        else if(pessoa.programacao == "Não gosta"){
+            document.getElementsByTagName("p")[1].innerHTML = `\nQue pena que não está gostando de programação... Já tentou aprender outras linguagens?`;
+        }
+    }
+
+    EnviarDados(){
+        let pessoa = this.LerDados();
+        if(this.ValidarCampos(pessoa)){
+            this.AdicionarNaArray(pessoa);
+            this.ExibirDados(pessoa);
+            this.LimparCampos();
+        }
+    }
 }
+
+const pessoa = new Pessoa();
